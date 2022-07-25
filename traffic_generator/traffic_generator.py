57ebcf4cc44bc6b7a90a059ce97b17b34e5a2f46
sehbeygi79 <sehbeygi79@gmail.com>
2022-07-25 21:32:36 +0430

    code added to make and send random SYN packets at about 1 ms intervals

--- traffic_generator/traffic_generator.py
+++ traffic_generator/traffic_generator.py
@@ -2,6 +2,16 @@
 from scapy.all import *
 from scapy.layers.inet import *
 
-pkts = sniff(offline="sample.pcap", count=2)
-print(pkts)
-sendp(pkts, iface="wlp3s0", loop=1, verbose=0)
+# pkts = sniff(offline="sample.pcap", count=2)
+# print(pkts)
+
+
+target_ip = "192.168.1.1"
+target_port = 80
+ip = IP(src=RandIP("192.168.1.1/16"), dst=target_ip)
+tcp = TCP(sport=RandShort(), dport=target_port, flags="S")
+
+p = Ether() / ip / tcp / Raw(RandString(size=1400))
+
+sendp(p, iface='lo', loop=1, verbose=0)
+# sendpfast(p, iface='lo', pps=100000, loop=100000)
