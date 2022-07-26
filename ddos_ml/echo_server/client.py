d059aa858ed4bbc88240af7cc54828cd4bd1d2cb
sehbeygi79 <sehbeygi79@gmail.com>
2022-07-26 18:40:15 +0430

    argparse added

--- ddos_ml/echo_server/client.py
+++ ddos_ml/echo_server/client.py
@@ -1,14 +1,25 @@
-# echo-client.py
+import argparse
 import socket
 from time import sleep
 
-HOST = "127.0.0.1"  # The server's hostname or IP address
-PORT = 65432  # The port used by the server
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--ip', dest='server_ip',
+                        help='server IP address', default='127.0.0.1')
+    parser.add_argument('--port', dest='server_port',
+                        help='server port', default='65432')
+    parser.add_argument('--text', '-t', dest='text',
+                        help='text to be sent to server', default='Hello World!')
+    args = parser.parse_args()
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    while True:
-        sleep(2)
-        s.sendall(b"Hello, world")
-        data = s.recv(1024)
-        print(f"Received {data!r}")
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((args.server_ip, int(args.server_port)))
+        while True:
+            try:
+                sleep(2)
+                s.send(bytes(args.text, 'utf-8'))
+                data = s.recv(1024)
+                print(f"Received \'{data.decode('utf-8')}\'")
+            except:
+                print('connection with server terminated')
+                break
