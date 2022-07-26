58699a54d7ac197176ea71ccc469913a09a6c653
sehbeygi79 <sehbeygi79@gmail.com>
2022-07-27 01:03:23 +0430

    epoch time fixed, capturing happens in a loop and after a while it draws the graphs

--- ddos_ml/sniff.py
+++ ddos_ml/sniff.py
@@ -2,46 +2,59 @@ from numpy import NAN
 from scapy.all import *
 from scapy.layers.inet import *
 import pandas as pd
+import time
 from stage1 import FlowDfGenerator
 
-# capture = sniff(iface='wlp3s0', filter='ip', count=1000)
-capture = sniff(iface='lo', filter='ip', timeout=5)
-# print(capture.summary())
 
-packet_list = []
-for packet in capture:
-    p = dict()
-    p['type'] = 'TCP' if TCP in packet else \
-        'UDP' if UDP in packet else \
-        'IP' if IP in packet else \
-        'None'
-    if p['type'] == 'None':
-        continue
-    elif p['type'] == 'TCP':
-        p['Source_Port'] = packet['TCP'].sport
-        p['Destination_Port'] = packet['TCP'].dport
-    elif p['type'] == 'UDP':
-        p['Source_Port'] = packet['UDP'].sport
-        p['Destination_Port'] = packet['UDP'].dport
-    else:
-        p['Source_Port'] = NAN
-        p['Destination_Port'] = NAN
+def sniff_packet_df(sniff_if, sniff_filter='ip', sniff_timeout=1):
+    # capture = sniff(iface='wlp3s0', filter='ip', count=1000)
+    capture = sniff(iface=sniff_if, filter=sniff_filter, timeout=sniff_timeout)
+    # print(capture.summary())
 
-    p['Time'] = packet.time
-    p['Source_ip'] = packet['IP'].src
-    p['Destination_IP'] = packet['IP'].dst
-    p['Frame_length'] = len(packet['IP'])
+    packet_list = []
+    for packet in capture:
+        p = dict()
+        p['type'] = 'TCP' if TCP in packet else \
+            'UDP' if UDP in packet else \
+            'IP' if IP in packet else \
+            'None'
+        if p['type'] == 'None':
+            continue
+        elif p['type'] == 'TCP':
+            p['Source_Port'] = packet['TCP'].sport
+            p['Destination_Port'] = packet['TCP'].dport
+        elif p['type'] == 'UDP':
+            p['Source_Port'] = packet['UDP'].sport
+            p['Destination_Port'] = packet['UDP'].dport
+        else:
+            p['Source_Port'] = NAN
+            p['Destination_Port'] = NAN
 
-    packet_list.append(p)
+        p['Time'] = packet.time - CURR_EPOCH_TIME
+        p['Source_ip'] = packet['IP'].src
+        p['Destination_IP'] = packet['IP'].dst
+        p['Frame_length'] = len(packet['IP'])
 
-packet_df = pd.DataFrame(packet_list)[["Time", "Source_ip", 'Source_Port', 'Destination_IP',
-                                       'Destination_Port', 'Frame_length']]
-print(packet_df)
+        packet_list.append(p)
 
+    return pd.DataFrame(packet_list)[["Time", "Source_ip", 'Source_Port', 'Destination_IP',
+                                      'Destination_Port', 'Frame_length']]
+
+
+CURR_EPOCH_TIME = int(time.time())
 flow_df_generator = FlowDfGenerator()
-flow_df = flow_df_generator.generate_flow_dataframe(packet_df, chunk_size=1000, is_labeled=False)
+flow_df = pd.DataFrame()
+while True:
+    packet_df = sniff_packet_df('lo')
+    # print(packet_df)
+    flow_df = pd.concat([flow_df, flow_df_generator.generate_flow_dataframe(
+        packet_df, chunk_size=20, is_labeled=False)])
+    print(flow_df)
+    if len(flow_df) > 3000:
+        break
+
 # saving
-# flow_df.to_csv('datasets/TCP_SYN_FLOODING.csv', index=False)
+flow_df.to_csv('output1.csv', index=False)
 plt.plot(flow_df['Mean_Time'], flow_df['SSIP'], color="green")
 plt.plot(flow_df['Mean_Time'], flow_df['SSP'], color="red")
 plt.show()
