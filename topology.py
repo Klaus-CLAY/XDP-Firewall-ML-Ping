e186c3c908ac531ecf8b18ba7ec57e2cf147d6d8
sehbeygi79 <sehbeygi79@gmail.com>
2022-07-27 22:37:51 +0430

    sample comnetsemu topology added

---
+++ topology.py
@@ -0,0 +1,65 @@
+import comnetsemu.tool as tool
+from comnetsemu.net import Containernet
+from mininet.link import TCLink
+from mininet.log import info, setLogLevel
+from mininet.node import Controller
+from comnetsemu.cli import CLI
+
+
+def testTopo():
+    "Create an empty network and add nodes to it."
+
+    net = Containernet(controller=Controller, link=TCLink)
+
+    info("*** Adding controller\n")
+    net.addController("c0")
+
+    info("*** Adding hosts\n")
+    center = net.addDockerHost(
+        "center",
+        dimage="sec_test",
+        ip="10.0.0.1",
+        docker_args={"cpuset_cpus": "1", "cpu_quota": 25000},
+    )
+    client1 = net.addDockerHost(
+        "client1",
+        dimage="sec_test",
+        ip="10.0.0.2",
+        docker_args={"cpuset_cpus": "1", "cpu_quota": 25000},
+    )
+    client3 = net.addDockerHost(
+        "client3",
+        dimage="sec_test",
+        ip="10.0.0.3",
+        docker_args={"cpuset_cpus": "0", "cpu_quota": 25000},
+    )
+
+    info("*** Adding switch\n")
+    s1 = net.addSwitch("s1")
+
+    info("*** Creating links\n")
+    net.addLinkNamedIfce(s1, center, bw=10, delay="10ms")
+    net.addLinkNamedIfce(s1, client1, bw=10, delay="10ms")
+    net.addLinkNamedIfce(s1, client3, bw=10, delay="10ms")
+
+    info("*** Starting network\n")
+    net.start()
+    CLI(net)
+
+    info("*** Create wg key pairs\n")
+    center_private_key = center.cmd("cat ./privatekey")
+
+    info("*** Create wg interfaces\n")
+
+    center.cmd(
+        "printf -- '[Interface]\nAddress = 192.168.0.1/24\nSaveConfig = true\nListenPort = 1337\nPrivateKey = "
+        + center_private_key + " > /etc/wireguard/wg0.conf"
+    )
+
+    info("*** Stopping network\n")
+    net.stop()
+
+
+if __name__ == "__main__":
+    setLogLevel("info")
+    testTopo()
