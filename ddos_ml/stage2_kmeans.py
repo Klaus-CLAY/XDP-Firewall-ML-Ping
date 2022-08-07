a300963ce432c7bdf8feed515f056d37829220da
sehbeygi79 <sehbeygi79@gmail.com>
2022-08-07 17:12:05 +0430

    function compare_results added

--- ddos_ml/stage2_kmeans.py
+++ ddos_ml/stage2_kmeans.py
@@ -3,29 +3,38 @@ from sklearn.cluster import KMeans
 from sklearn.model_selection import train_test_split
 import pickle
 
-def compare_results(a, b):
-    cmp = []
-    for i in range(len(a)):
-        if a[i] != b[i]:
-            cmp.append(i)
-    
-    return cmp
+def compare_results(actual_res, predicted_res):
+    assert(len(actual_res) == len(predicted_res))
+    l = len(predicted_res)
+    tn, tp, fn, fp = 0, 0, 0, 0
+
+    for i in range(l):
+        if actual_res[i] == predicted_res[i]:
+            if predicted_res[i] == 1:
+                tp += 1
+            else:
+                tn += 1
+        else:
+            if predicted_res[i] == 1:
+                fp += 1
+            else:
+                fn += 1
+    # print(f'tp: {tp}\ttn: {tn}\tfp: {fp}\tfn: {fn}')
+    print(f'precision: {tp/(tp+fp)}')
+    print(f'accuracy: {(tp + tn)/l}')
+    print(f'sensitivity (TPR): {tp/(tp+fn)}')
+    print(f'specificity (TNR): {tn/(tn+fp)}')
+
 
 
 if __name__ == '__main__':
     flow_df = pd.read_csv(r'dump_4clients_0.2interval_fabbed.csv')
     x = flow_df[flow_df.columns.difference(['Traffic_Type', 'Mean_Time'])]
     y = flow_df['Traffic_Type']
-
     x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=0, train_size = .65)
-
-    print(x_train)
-    print()
-    # print(y_train)
 
     model = KMeans(n_clusters=2)
     model.fit(x_train)
-    # dtree.save('dt_model.h5')
 
     # save
     with open('kmeans_model.pkl','wb') as f:
@@ -35,14 +44,10 @@ if __name__ == '__main__':
         loaded_model = pickle.load(f)
 
     y_train_predict = loaded_model.predict(x_train)
-    diff = compare_results(list(y_train), list(y_train_predict))
-    print(f'accuracy train: {(1 - len(diff) / len(y_train))*100}%')
+    print('train stats:')
+    compare_results(list(y_train), list(y_train_predict))
 
     y_test_predict = loaded_model.predict(x_test)
-    diff = compare_results(list(y_test), list(y_test_predict))
-    print(f'accuracy test: {(1 - len(diff) / len(y_test))*100}%')
-    # print(list(y_test_predict))
-    # print('vs')
-    # print(list(y_test))
-
+    print('\ntest stats:')
+    compare_results(list(y_test), list(y_test_predict))
 
